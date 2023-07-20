8d0619d0e99471b797823405a9dfb380e77dd7c8
shmishra99 <shivammishr@google.com>
2023-07-20 18:37:23 +0530

    commit

--- .github/workflows/repoArch.js
+++ .github/workflows/repoArch.js
@@ -4,7 +4,7 @@ module.exports = async ({ github, context }) => {
   for (let i = 1; i < 4; i++) {
     let reposData = await github.rest.repos.listForOrg({
       org: "tensorflow",
-      per_page: 100,
+      per_page: 10,
       page: i,
     });
     const repos = reposData.data;
@@ -13,10 +13,10 @@ module.exports = async ({ github, context }) => {
       let eventsData = await github.rest.activity.listRepoEvents({
         owner: "tensorflow",
         repo: repo.name,
-        per_page: 100,
+        per_page: 2,
         page: i,
       });
-
+      console.log("Event",eventsData)
       let events = eventsData.data;
       let lastEvent = events[0];
       events.push(lastEvent);
