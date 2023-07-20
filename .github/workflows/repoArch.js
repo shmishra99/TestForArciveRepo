62d3f44f1791b36f2e8e265b513403324a7f1683
shmishra99 <shivammishr@google.com>
2023-07-21 01:08:16 +0530

    commit

--- .github/workflows/repoArch.js
+++ .github/workflows/repoArch.js
@@ -1,6 +1,7 @@
 module.exports = async ({ github, context }) => {
-  let arr = [];
+  let inactiveRepos = [];
   let numberOfDaysInactive = 90;
+  
 
   for (let i = 1; i < 4; i++) {
     //fetch all the repos from 'tensorflow organization'
@@ -12,9 +13,14 @@ module.exports = async ({ github, context }) => {
     const repos = reposData.data;
 
     for (let repo of repos) {
+      let repoObj  = {
+         repo_details: repo,
+      }
+      let getTimeDiffEvent;
+      let timeDifferneceRelese; 
+      
       // List all the pull request and issues identify pull request by 'pull_request' key sort by update_at
       //It will also cover comment event.
-
       let listRepoIssueData = await github.rest.issues.listForRepo({
         owner: "tensorflow",
         repo: repo.name,
@@ -22,13 +28,8 @@ module.exports = async ({ github, context }) => {
       });
 
       let listRepoIssue = listRepoIssueData.data[0];
-      console.log("listRepoIssueData", listRepoIssue); // updated_at,
-      // let getTimeDiff = timeDiffernece(listRepoIssue.updated_at);
-  
-      
+      getTimeDiffEvent = timeDiffernece(listRepoIssue.updated_at);
       // fetch the last update date if it is less then 90 days then ignore else archive and continue.
-      console.log("listRepoIssueData", listRepoIssue);
-
       try {
         //get the latest relesedata
         let getLatestReleaseData = await github.rest.repos.getLatestRelease({
@@ -36,14 +37,30 @@ module.exports = async ({ github, context }) => {
           repo: repo.name,
         });
         let getLatestRelease = getLatestReleaseData.data;
-        console.log("getLatestRelease", getLatestRelease);
+         timeDifferneceRelese =  timeDiffernece(getLatestRelease.updated_at)
       } catch (e) {
         console.log("no relese.");
       }
+      
+      if(numberOfDaysInactive < getTimeDiffEvent && numberOfDaysInactive < timeDifferneceRelese){
+          
+           if(getTimeDiffEvent < timeDifferneceRelese)
+              {
+               repoObj["inactiveDays"] = getTimeDiffEvent
+              }
+              else 
+              repoObj["inactiveDays"] = timeDifferneceRelese
+         
+
+      }
+      if(repoObj["inactiveDays"])
+         inactiveRepos.push(repoObj)
     }
+    
+
   }
 
-  //   console.log("events Arrya", eventsArr);
+    console.log("events Arrya", inactiveRepos);
 };
 
 function timeDiffernece(updateDate) {
@@ -51,5 +68,5 @@ function timeDiffernece(updateDate) {
   let todaysDate = new Date().getTime();
   let differenceInSec = todaysDate - updateTime;
   let daysDifference = differenceInSec / 86400000;
-  console.log("Diff", daysDifference);
+  return daysDifference;
 }
