866b3f87e7e61b7fb6611f8eb31af8c862b2f1a8
shmishra99 <shivammishr@google.com>
2023-07-21 00:37:00 +0530

    commit

--- .github/workflows/repoArch.js
+++ .github/workflows/repoArch.js
@@ -1,6 +1,5 @@
 module.exports = async ({ github, context }) => {
   let arr = [];
-  let eventsArr = [];
   let numberOfDaysInactive = 90;
 
   for (let i = 1; i < 4; i++) {
@@ -11,38 +10,45 @@ module.exports = async ({ github, context }) => {
       page: i,
     });
     const repos = reposData.data;
-   
+
     for (let repo of repos) {
-     
       // List all the pull request and issues identify pull request by 'pull_request' key sort by update_at
-      //It will also cover comment event. 
-      
+      //It will also cover comment event.
+
       let listRepoIssueData = await github.rest.issues.listForRepo({
         owner: "tensorflow",
         repo: repo.name,
-        sort:"updated"
+        sort: "updated",
       });
+
       let listRepoIssue = listRepoIssueData.data[0];
-      console.log("listRepoIssueData",listRepoIssue)
-      
+      console.log("listRepoIssueData", listRepoIssue); // updated_at,
+      // let getTimeDiff = timeDiffernece(listRepoIssue.updated_at);
+  
+      // fetch the last update date if it is less then 90 days then ignore else archive and continue.
+      console.log("listRepoIssueData", listRepoIssue);
 
-     
-      try{
-      //get the latest relesedata
-      let getLatestReleaseData = await github.rest.repos.getLatestRelease({
-        owner: "tensorflow",
-        repo: repo.name,
-      });
-      let getLatestRelease = getLatestReleaseData;
-      console.log("getLatestRelease",getLatestRelease)
+      try {
+        //get the latest relesedata
+        let getLatestReleaseData = await github.rest.repos.getLatestRelease({
+          owner: "tensorflow",
+          repo: repo.name,
+        });
+        let getLatestRelease = getLatestReleaseData.data;
+        console.log("getLatestRelease", getLatestRelease);
+      } catch (e) {
+        console.log("no relese.");
       }
-     catch(e){
-           console.log("no relese.")
-      }
-      
-      
-
     }
   }
-//   console.log("events Arrya", eventsArr);
+
+  //   console.log("events Arrya", eventsArr);
 };
+
+function timeDiffernece(updateDate) {
+  let updateTime = new Date(updateDate).getTime();
+  let todaysDate = new Date().getTime();
+  let differenceInSec = todaysDate - updateTime;
+  let daysDifference = differenceInSec / 86400000;
+  console.log("Diff", daysDifference);
+}
