26f6847d19aef0323064b86a9ab535c548ffdd4c
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2023-07-20 16:49:13 +0530

    Update repoArch.js

--- .github/workflows/repoArch.js
+++ .github/workflows/repoArch.js
@@ -2,11 +2,15 @@
 
 module.exports = async ({ github, context }) => {
 
+   let arr = [];
+   for(let i=1;i<10;i++){
    let repos =   await github.rest.repos.listForOrg({
                     org: "tensorflow",
                    
                 })
-   console.log("rep",repos.data.length)
+      arr.push(...repos.data)
+   }
+   console.log("rep",arr.length)
    
    
    
