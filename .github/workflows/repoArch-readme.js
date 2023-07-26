f1749d45d1d7ce4e6ddb93b89e75e72c1e215e44
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2023-07-26 14:43:17 +0530

    Update repoArch-readme.js

--- .github/workflows/repoArch-readme.js
+++ .github/workflows/repoArch-readme.js
@@ -5,7 +5,7 @@ module.exports = async ({ github, context }) => {
   github.rest.repos.createOrUpdateFileContents({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    path:'README.md',
+    path:'README1.md',
     message:"commit to branch message",
     content:"SGkgaSBhbSB3cml0aW5nIHRoZSBjb250ZW50",
     committer:{
