03bb74117099626c2492c12a5b466e5e0b63ea8f
shmishra99 <shivammishr@google.com>
2023-07-26 14:15:46 +0530

    push

---
+++ .github/workflows/repoArch-readme.js
@@ -0,0 +1,22 @@
+let numberOfDaysInactive = 90;
+
+module.exports = async ({ github, context }) => {
+
+  github.rest.repos.createOrUpdateFileContents({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    path:'./README.md',
+    message:"commit to branch message",
+    content:"Commit content.",
+    committer:{
+    name:"Shivam Mishra",
+    email:"shivammishr@google.com"
+    },
+    author:{
+    name:"Shivam Mishra",
+    email:"shivammishr@google.com"
+    }
+    
+     })
+
+}
